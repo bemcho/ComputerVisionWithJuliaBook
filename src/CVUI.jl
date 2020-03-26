6d662e616758a19ba3c29eab8feab990054162da
Emil Tomov <bemcho@gmail.com>
2020-03-26 15:14:18 +0200

    Added section for Dimensions edit, open,save dialogs now have image mime type filters

--- src/CVUI.jl
+++ src/CVUI.jl
@@ -1,19 +1,32 @@
 module CVUI
 export initUI
+const image_filters = ("*.png", "*.jpg", "*.gif", "*.tiff")
 
 include("CVProcessing.jl")
 
 using Gtk, ImageView, .CVProcessing
 
 function open_file(w)
-    path = open_dialog("Open image file")
+    path = open_dialog("Open image file", GtkNullContainer(), image_filters)
     processedImage = CVProcessing.open_file(path)
     imshow!(canvas, processedImage)
 end
 
 function save_file(w)
-    path = save_dialog("Save image file")
+    path = save_dialog("Save image file", GtkNullContainer(), image_filters)
     CVProcessing.save_file(path, processedImage)
+end
+
+function resize_image(w)
+    println("Resize image called")
+end
+
+function crop_image(w)
+    println("Crop image called")
+end
+
+function scale_image(w)
+    println("Scale image called")
 end
 
 function initUI()
@@ -35,11 +48,15 @@ function initUI()
 
     imshow!(canvas, processedImage)
 
-    openFileBtn = builder["open_image_btn"]
-    saveFileBtn = builder["save_image_btn"]
+    # open/save
+    connect_button(open_file, builder["open_image_btn"], "clicked", true)
+    connect_button(save_file, builder["save_image_btn"], "clicked", true)
 
-    signal_connect(open_file, openFileBtn, "clicked", true)
-    signal_connect(save_file, saveFileBtn, "clicked", true)
+    # Dimensions
+    connect_button(resize_image, builder["image_dimensions_resize_btn"], "clicked", true)
+    connect_button(crop_image, builder["image_dimensions_crop_btn"], "clicked", true)
+    connect_button(scale_image, builder["image_dimensions_scale_btn"], "clicked", true)
+
 
     showall.([winDraw, winToolbar])
 
@@ -51,4 +68,7 @@ function initUI()
         wait(c)
     end
 end
+
+connect_button(handler, btn, event, after = true) =
+    signal_connect(handler, btn, event, after)
 end
